f817b44069584cebad110d9f927feda4ab4343df
MusaMahmood <musasmahmood@gmail.com>
2018-11-19 14:34:37 -0500

    Updated data_compiler for new dataset (test/train)

--- data/data_compiler.py
+++ data/data_compiler.py
@@ -5,8 +5,9 @@ from tf_shared import load_data_v2, prep_dir, ind2vec
 num_classes = 5
 seq_length = 2000
 input_length = seq_length
-dir_1 = 'incartdb_5c_shift'
-dir_2 = 'ptb_5c_shift'
+suffix = '_train'
+dir_1 = 'incartdb_5c_shift' + suffix
+dir_2 = 'ptb_5c_shift' + suffix
 dir_out = 'incart_ptb_5c'
 key_x = 'X'
 key_y = 'Y'
@@ -19,6 +20,4 @@ x_data = np.concatenate([x_data, x_data2], axis=0)
 y_data = np.concatenate([y_data, y_data2], axis=0)
 print("Loaded Data Shape Final: X:", x_data.shape, " Y: ", y_data.shape)
 
-savemat(prep_dir(dir_out + '_all/') + 'all_data.mat', mdict={key_x: x_data, key_y: y_data})
-
-
+savemat(prep_dir(dir_out + '_all' + suffix + '/') + 'all_data.mat', mdict={key_x: x_data, key_y: y_data})
